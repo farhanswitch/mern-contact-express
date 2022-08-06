4bb2c4ee1f50776984798a68dbeb034677a2b12e
farhanswitch <muhammad_farhan@outlook.co.id>
2022-08-06 07:28:59 -0700

    setup basic app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const PORT = 4000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost", "http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
