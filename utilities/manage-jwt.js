011c0afdebe93dfe3b5133a2b2064afe860d9791
farhanswitch <muhammad_farhan@outlook.co.id>
2022-08-06 07:33:53 -0700

    add utilities/manage-jwt.js

---
+++ utilities/manage-jwt.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (id) => {
+  return jwt.sign(
+    {
+      id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "5m",
+    }
+  );
+};
+
+const verifyJWT = (req, res, next) => {
+  const token = req?.cookies?.fstoken;
+  if (!token) {
+    res.json({
+      statusMsg: "Error",
+      errors: [{ msg: "You have no credential. Please Login" }],
+    });
+  } else {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, result) => {
+      if (err) {
+        res.json({
+          statusMsg: "Error",
+          errors: [{ msg: "Invalid Credentials" }],
+        });
+      } else {
+        // console.log(result);
+        req.userId = result.id;
+        next();
+      }
+    });
+  }
+};
+
+module.exports = { generateJWT, verifyJWT };
